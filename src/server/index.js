ce6af68a51032912691c0e58cff14f19494a2758
Vineeta P <vineeta.prashar@gmail.com>
2023-06-26 08:34:16 -0600

    API keys setup done

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,16 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+var textapi = new meaningcloud({    
+    application_key: process.env.API_KEY
+});
+
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+
 const app = express()
 
 app.use(express.static('dist'))
